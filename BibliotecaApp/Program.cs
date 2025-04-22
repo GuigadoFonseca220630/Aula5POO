0afaf6e7baac5315b918fd5215d4e7e1fe99b946
GuigadoFonseca220630 <g.f.oliva@uni9.edu.br>
2025-04-22 20:33:28 -0300

    Update Program.cs

--- BibliotecaApp/Program.cs
+++ BibliotecaApp/Program.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace BibliotecaAppRefatorado
+namespace BibliotecaApp
 {
     // Interface de Notificação
     public interface INotificacaoService
